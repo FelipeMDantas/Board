b0d48db8801d1cacb37c5b67cc2141c652289752
FelipeMDantas <felipemdantas@terra.com.br>
2024-02-29 23:14:15 -0300

    Liveblocks API developed.

--- app/board/[boardId]/_components/info.tsx
+++ app/board/[boardId]/_components/info.tsx
@@ -1,12 +1,51 @@
-export const Info = () => {
+"use client";
+
+import { Hint } from "@/components/hint";
+import { Button } from "@/components/ui/button";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { cn } from "@/lib/utils";
+import { useQuery } from "convex/react";
+import { Poppins } from "next/font/google";
+import Image from "next/image";
+import Link from "next/link";
+
+interface InfoProps {
+  boardId: string;
+}
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
+
+export const Info = ({ boardId }: InfoProps) => {
+  const data = useQuery(api.board.get, { id: boardId as Id<"boards"> });
+
+  if (!data) return <InfoSkeleton />;
+
   return (
     <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md">
-      Info about the board
+      <Hint label="Go to boards" side="bottom" sideOffset={10}>
+        <Button asChild className="px-2" variant={"board"}>
+          <Link href={"/"}>
+            <Image src={"/logo.svg"} alt="Board logo" height={40} width={40} />
+            <span
+              className={cn(
+                "font-semibold text-xl ml-2 text-black",
+                font.className
+              )}
+            >
+              Board
+            </span>
+          </Link>
+        </Button>
+      </Hint>
     </div>
   );
 };
 
-Info.Skeleton = function InfoSkeleton() {
+export const InfoSkeleton = () => {
   return (
     <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md w-[300px]" />
   );
